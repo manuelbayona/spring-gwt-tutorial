1e2bedf3beb90d53688c3fbb9e8a60048367930d
MBayona <manuel.bayona.adesis@gmail.com>
2015-04-23 09:41:46 +0200

    [ADD] refactor

--- src/main/java/com/adesis/gwt/server/GreetingServiceImpl.java
+++ src/main/java/com/adesis/gwt/server/GreetingServiceImpl.java
@@ -5,47 +5,42 @@ import org.springframework.stereotype.Service;
 
 import com.adesis.gwt.client.GreetingService;
 import com.adesis.gwt.shared.FieldVerifier;
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 /**
  * The server side implementation of the RPC service.
  */
-@SuppressWarnings("serial")
 @Service("greetingService")
-public class GreetingServiceImpl extends RemoteServiceServlet implements
-    GreetingService {
+public class GreetingServiceImpl implements GreetingService {
 
-  @Autowired
-  private SpringService springService;
-	  
-  public String greetServer(String input) throws IllegalArgumentException {
-    // Verify that the input is valid.
-    if (!FieldVerifier.isValidName(input)) {
-      // If the input is not valid, throw an IllegalArgumentException back to
-      // the client.
-      throw new IllegalArgumentException(
-          "Name must be at least 4 characters long");
-    }
+	@Autowired
+	private SpringService springService;
 
-    // Escape data from the client to avoid cross-site script vulnerabilities.
-    input = escapeHtml(input);
+	public String greetServer(String input) throws IllegalArgumentException {
+		// Verify that the input is valid.
+		if (!FieldVerifier.isValidName(input)) {
+			// If the input is not valid, throw an IllegalArgumentException back to
+			// the client.
+			throw new IllegalArgumentException("Name must be at least 4 characters long");
+		}
 
-    // Delegate to SpringService and return the result
-    return springService.echo(input);
-  }
+		// Escape data from the client to avoid cross-site script vulnerabilities.
+		input = escapeHtml(input);
 
-  /**
-   * Escape an html string. Escaping data received from the client helps to
-   * prevent cross-site script vulnerabilities.
-   *
-   * @param html the html string to escape
-   * @return the escaped string
-   */
-  private String escapeHtml(String html) {
-    if (html == null) {
-      return null;
-    }
-    return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(
-        ">", "&gt;");
-  }
+		// Delegate to SpringService and return the result
+		return springService.echo(input);
+	}
+
+	/**
+	 * Escape an html string. Escaping data received from the client helps to prevent cross-site script vulnerabilities.
+	 *
+	 * @param html
+	 *            the html string to escape
+	 * @return the escaped string
+	 */
+	private String escapeHtml(final String html) {
+		if (html == null) {
+			return null;
+		}
+		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+	}
 }
